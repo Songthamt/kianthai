782b8d95716c184b3764367f2f6a6a95062d2826
Songthamt <songthamtung@gmail.com>
2026-04-19 22:22:36 +0700

    remove dist. add to .gitignore

--- dist/server/assets/_tanstack-start-manifest_v-CLhIeYyw.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/songthamtung/Desktop/projects/kianthai/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/kianthai/assets/index-D95JGdQK.js"] }, "/": { filePath: "/Users/songthamtung/Desktop/projects/kianthai/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/kianthai/assets/index-C1ptirox.js"] } }, clientEntry: "/kianthai/assets/index-D95JGdQK.js" });
-export {
-  tsrStartManifest
-};
